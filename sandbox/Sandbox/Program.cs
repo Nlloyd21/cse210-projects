4ad2a32af6e1161965f8d75ca70b2624e0a0e6cf
Nlloyd21 <llo21008@byui.edu>
2025-04-29 09:44:53 -0600

    added and updated the sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        int x = 10;
+
+        for (int i = 0; i <= x; i++)
+        {
+            Console.WriteLine("" + i);
+        } 
     }
 }
