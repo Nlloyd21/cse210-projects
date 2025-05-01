56593dc19c8b4265b34535f9b37853f1e296ada0
Nlloyd21 <llo21008@byui.edu>
2025-05-01 11:09:30 -0600

    class examples added to the sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,15 +3,37 @@ using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+
+    static double AddNumbers(double a, double b)
+        {
+            return a + b;
+        }
+
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
 
-        int x = 10;
+        // int x = 10;
 
-        for (int i = 0; i <= x; i++)
-        {
-            Console.WriteLine("" + i);
-        } 
+        // for (int i = 0; i <= x; i++)
+        // {
+        //     Console.WriteLine("" + i);
+        // } 
+
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(10);
+        // myNumbers.Add(9);
+        // myNumbers.Add(100);
+
+        // foreach (int i in myNumbers)
+        // {
+        //     Console.WriteLine(i);
+        // }
+        // Console.WriteLine(myNumbers.Count);
+
+        double total = AddNumbers(234, 432);
+        Console.WriteLine(total);
+        
     }
 }
