a99bd01f195c78aced46e4a9b7ecf565307a859d
Nlloyd21 <llo21008@byui.edu>
2025-05-06 09:59:14 -0600

    added a circle class with objects

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,39 +1,60 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 
+
+class Circle
+{
+    private double _radius;
+
+    public void setRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
 class Program
 {
 
-    static double AddNumbers(double a, double b)
-        {
-            return a + b;
-        }
-
-
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+    Circle myCircle = new Circle();
+    myCircle.setRadius(10);
 
-        // int x = 10;
-
-        // for (int i = 0; i <= x; i++)
-        // {
-        //     Console.WriteLine("" + i);
-        // } 
-
-        // List<int> myNumbers = new List<int>();
-        // myNumbers.Add(10);
-        // myNumbers.Add(9);
-        // myNumbers.Add(100);
-
-        // foreach (int i in myNumbers)
-        // {
-        //     Console.WriteLine(i);
-        // }
-        // Console.WriteLine(myNumbers.Count);
-
-        double total = AddNumbers(234, 432);
-        Console.WriteLine(total);
-        
+    Console.WriteLine($"{myCircle.GetRadius()}"); 
+    Console.WriteLine($"{myCircle.GetArea()}");
     }
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Sandbox World!");
+
+//         // int x = 10;
+
+//         // for (int i = 0; i <= x; i++)
+//         // {
+//         //     Console.WriteLine("" + i);
+//         // } 
+
+//         // List<int> myNumbers = new List<int>();
+//         // myNumbers.Add(10);
+//         // myNumbers.Add(9);
+//         // myNumbers.Add(100);
+
+//         // foreach (int i in myNumbers)
+//         // {
+//         //     Console.WriteLine(i);
+//         // }
+//         // Console.WriteLine(myNumbers.Count);
+
+//         double total = AddNumbers(234, 432);
+//         Console.WriteLine(total);
+        
+//     }
 }
