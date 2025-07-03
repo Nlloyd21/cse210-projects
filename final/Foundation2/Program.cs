0889c5e973c8b257dab62556464574f9ab37e397
Nlloyd21 <llo21008@byui.edu>
2025-07-03 13:13:13 -0600

    Created the program and added different customers, address, and products.

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address("1600 Natures Way", "Lindenhurst", "IL", "USA");
+        Address address2 = new Address("22 Ballard Rd", "Pelham", "NH", "USA");
+        Address address3 = new Address("241 Scary Dr", "London", "Endland", "UK");
+
+        Customer customer1 = new Customer("Noah Lloyd", address3);
+        Customer customer2 = new Customer("Raj Patil", address2);
+        Customer customer3 = new Customer("Giselle", address1);
+
+        Product product1 = new Product("Laptop", "LP-351", 255, 1);
+        Product product2 = new Product("Microphone", "MM-111", 10, 1);
+        Product product3 = new Product("Colored Pencils", "CC-142", 5.99, 5);
+        Product product4 = new Product("Erasers", "ST-143", 0.69, 3);
+
+        Order order1 = new Order(customer1);
+        order1.Add(product1);
+        order1.Add(product2);
+
+        Order order2 = new Order(customer2);
+        order2.Add(product3);
+        order2.Add(product4);
+        order2.Add(product1);
+
+        Order order3 = new Order(customer3);
+        order3.Add(product2);
+        order3.Add(product1);
+
+        Console.WriteLine("----------------------");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: {order1.GetTotalCost()}");
+
+        Console.WriteLine("\n----------------------");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: {order2.GetTotalCost()}");
+        
+        Console.WriteLine("\n----------------------");
+        Console.WriteLine(order3.GetPackingLabel());
+        Console.WriteLine(order3.GetShippingLabel());
+        Console.WriteLine($"Total Price: {order3.GetTotalCost()}");
     }
 }
