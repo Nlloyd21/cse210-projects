0af5d5da3b13ad86b2ac2f7cf85848e9a558b822
Nlloyd21 <llo21008@byui.edu>
2025-07-03 15:36:26 -0600

    Created the main program. Activities are in a list. A foreach is then run to cycle through the list and display the GetSummary() for each activity.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2025, 11, 4), 45, 15.0),
+            new Swimming(new DateTime(2025, 11, 5), 40, 60)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
